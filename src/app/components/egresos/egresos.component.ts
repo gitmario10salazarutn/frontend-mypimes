cbb28bedaa648b4040f01b6825bb4d9dd4f215ce
Luis Diaz <lpdiazd@utn.edu.ec>
2023-02-24 05:41:59 -0500

    Crud de Egresos Actualizado

--- src/app/components/egresos/egresos.component.ts
+++ src/app/components/egresos/egresos.component.ts
@@ -78,4 +78,52 @@ export class EgresosComponent implements OnInit {
     )
   }
 
+
+
+  //////////////////////////////////7
+
+  public infoUpdateEgreso(egre_id:any, tes_idtesorero:any, egre_descripcion:any, egre_subtotal:any, egre_iva:any, egre_total: any, egre_fecha:any, egre_numero:any, egre_estado_delete:any) {
+
+    this.informacionEgresos.egre_id = egre_id,
+    this.informacionEgresos.tes_idtesorero = tes_idtesorero,
+    this.informacionEgresos.egre_descripcion = egre_descripcion,
+    this.informacionEgresos.egre_subtotal= egre_subtotal,
+    this.informacionEgresos.egre_iva = egre_iva,
+    this.informacionEgresos.egre_total = egre_total,
+    this.informacionEgresos.egre_fecha = egre_fecha,
+    this.informacionEgresos.egre_numero= egre_numero,
+    this.informacionEgresos.egre_estado_delete= egre_estado_delete
+    
+}
+
+public actualizarEgreso(egre_id: any) {
+  console.log(egre_id+"....");
+  this.egresosService.putUpdateEgreso({
+
+    egre_id: this.form.value.txtegre_id,
+    tes_idtesorero: this.form.value.txttes_idtesorero,
+    egre_descripcion: this.form.value.txtegre_descripcion,
+    egre_subtotal: this.form.value.txtegre_subtotal,
+    egre_iva: this.form.value.txtegre_iva,
+    egre_total: this.form.value.txtegre_total,
+    egre_fecha: this.form.value.txtegre_fecha,
+    egre_numero: this.form.value.txtegre_numero,
+    egre_estado_delete: this.form.value.txtegre_estado_delete
+
+
+  },egre_id).subscribe(
+    respuesta => {
+      console.log('Egreso actualizado correctamente');
+
+      this.form.reset()
+      this.cargarEgresos()
+
+    }
+  )
+}
+
+
+
+  
+
 }
